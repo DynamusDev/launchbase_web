25ebe1a4fd6efea01d30948748860840bba0a98e
Alexandre Nascimento <alexandre.nascimento@nexodata.com.br>
2022-01-02 20:51:27 -0300

    novo launchbase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { Flex } from '../components/UI/Flex';
 
 export default function Home() {
   return (
